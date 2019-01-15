6789cb5a8280e4a093df5fe2f730657352046204
walter.maciel <walter.maciel@gmail.com>
2019-01-15 00:14:31 -0500

    update test.py and index

--- test.py
+++ test.py
@@ -19,7 +19,7 @@ class Test(unittest.TestCase):
         result = self.app.get('/') 
 
         # verifica o retorno do conteudo da pagina
-        self.assertRegex(result.data.decode(), "olá devops.")
+        self.assertRegex(result.data.decode(), "ola devops.")
 
 
 if __name__ == "__main__":
